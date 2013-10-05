6e529e4ce6e06269f2a85a6a02e080dc6ead78fa
Rafael Grillo <rafael@Rafaels-MacBook-Pro.local>
2013-10-05 00:11:48 -0300

    Ruby server file

---
+++ server/server.rb
@@ -0,0 +1,25 @@
+# require 'pry'
+require 'em-websocket'
+
+clients = Array.new
+EventMachine::WebSocket.start(host: "127.0.0.1", port: 5000) do |ws|
+  ws.onopen do |handshake|
+    puts "WebSocket opened #{{
+      :path => handshake.path,
+      :query => handshake.query,
+      :origin => handshake.origin,
+    }}"
+  end
+
+  ws.onmessage do |msg|
+    ws.send msg
+  end
+
+  ws.onclose do
+    puts "WebSocket closed"
+  end
+
+  ws.onerror do |e|
+    puts "Error: #{e.message}"
+  end
+end
